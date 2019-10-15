9ce47dbfb47f3836c6aac9217ad061bc9a4ce6a7
Corey <ckschanker@gmail.com>
2019-10-15 17:16:47 -0600

    Added V1 of Encoder-Class

--- include/robot/devices/encoderClass.hpp
+++ include/robot/devices/encoderClass.hpp
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "robot/devices/timerClass.hpp"
 
 #ifndef ENCODERCLASS_H
 #define ENCODERCLASS_H
@@ -6,9 +6,25 @@
 class Encoder{
 private:
   std::string m_name = "Encoder";
-  short m_port;
+  short m_port;// 1,3,5,7
+  Timer m_timer;
+
+  int m_zeroPosition;
+  int m_previousTime = 0;
+  int m_previousPosition = 0;
+  int m_rotation;
+  int m_velocity;
+
 public:
+  Encoder(const std::string p_name ,const int p_port);
+
+  bool isConnected();
+
   int getRotation();
+
+  int resetRotation();
+
+  int getVelocity();
 
   int defineGUI();
 };
--- include/robot/subsystems/baseSubsystems/baseClass.hpp
+++ include/robot/subsystems/baseSubsystems/baseClass.hpp
@@ -1,27 +1,41 @@
 #include "main.h"
 #include "robot/devices/motorClass.hpp"
+#include "robot/devices/encoderClass.hpp"
 #include "robot/devices/controller/controllerClass.hpp"
+#include "robot/graphical/GUIClass.hpp"
 
 #ifndef BASECLASS_H
 #define BASECLASS_H
 
+enum BaseType {
+	HOLONOMIC = 0,
+	HBASE = 1,
+	MECANUM = 2,
+	TANK = 3
+};
+
 class Base{
 protected:
-  Motor* m_frontLeft;
-  Motor* m_frontRight;
-  Motor* m_backLeft;
-  Motor* m_backRight;
-  Motor* m_center;
+  Motor* m_frontLeftMotor;
+  Motor* m_frontRightMotor;
+  Motor* m_backLeftMotor;
+  Motor* m_backRightMotor;
+  Motor* m_centerMotor;
 
+  Encoder* m_leftEncoder;
+  Encoder* m_centerEncoder;
+  Encoder* m_rightEncoder;
+
+  Controller* m_driveController;
+  Controller* m_partnerController;
+
+  ExternalFile* m_config;
+
+  bool m_trackingSystem = true;
+  BaseType m_baseType;
   
-
-  Controller m_driveController;
-
-  int calculatePosition();
-
   Base();
 
-  int drive();
 
 };
 
