caf575492ba7abf09319b00ba6b98749f7d17c79
Corey <ckschanker@gmail.com>
2019-10-20 14:15:59 -0600

    Working on temp holo code

--- src/robotClass.cpp
+++ src/robotClass.cpp
@@ -11,11 +11,29 @@ Robot::Robot():
       m_baseType = m_config.readString("Base_Type");
     }
     defineGUI();
+
+    m_mainController.Axis1.setMultiplier(1);
+    m_mainController.Axis2.setMultiplier(2);
+    m_mainController.Axis3.setMultiplier(2);
+    m_mainController.Axis4.setMultiplier(2);
 }
+
+Motor FR_Motor("FR", 1, pros::E_MOTOR_GEARSET_18, true);
+Motor FL_Motor("FL", 2, pros::E_MOTOR_GEARSET_18, false);
+Motor BR_Motor("BR", 3, pros::E_MOTOR_GEARSET_18, true);
+Motor BL_Motor("BL", 4, pros::E_MOTOR_GEARSET_18, false);
 
 int Robot::task(){
   m_gui.task();
   m_mainController.callBackCheck();
   m_partnerController.callBackCheck();
+
+  int turnFactorLeft = 1 + (m_mainController.Axis1.getValue()/100);
+  int turnFactorRight = 1 - (m_mainController.Axis1.getValue()/100);
+
+  FR_Motor.setVelocity((m_mainController.Axis3.getValue()-m_mainController.Axis4.getValue()) *turnFactorLeft);
+  FL_Motor.setVelocity((m_mainController.Axis3.getValue()+m_mainController.Axis4.getValue()) *turnFactorRight);
+  BR_Motor.setVelocity((m_mainController.Axis3.getValue()+m_mainController.Axis4.getValue()) *turnFactorLeft);
+  BL_Motor.setVelocity((m_mainController.Axis3.getValue()-m_mainController.Axis4.getValue()) *turnFactorRight);
   return 0;
 }
