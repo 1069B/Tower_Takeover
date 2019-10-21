19bc35d4e532003baeb52d7dc0d8dbbdb2ac0bfb
Corey <ckschanker@gmail.com>
2019-10-20 20:34:02 -0600

    Working Prototype for protorobot

--- src/robot/devices/encoderClass.cpp
+++ src/robot/devices/encoderClass.cpp
@@ -1,29 +1,30 @@
 #include "robot/devices/encoderClass.hpp"
 
-Encoder::Encoder(const std::string p_name ,const int p_port){
+RedEncoder::RedEncoder(const std::string p_name ,const int p_port){
   m_name = p_name;
   m_port = p_port;
+  pros::c::adi_encoder_init(m_port, m_port+1, false);
 }
 
-bool Encoder::isConnected(){
+bool RedEncoder::isConnected(){
   if(getRotation() == 2147483647)
     return true;
   return false;
 }
 
-int Encoder::getRotation(){
-  m_rotation = pros::c::adi_encoder_get(pros::c::adi_encoder_t(m_port)) - m_zeroPosition;// Add gettter methoud
+int RedEncoder::getRotation(){
+  m_rotation = pros::c::adi_encoder_get(m_port);
   return m_rotation;
 }
 
-int Encoder::resetRotation(){
-  m_zeroPosition = pros::c::adi_encoder_get(pros::c::adi_encoder_t(m_port));
+int RedEncoder::resetRotation(){
+  m_zeroPosition = pros::c::adi_encoder_get(m_port);
   return 0;
 }
 
-int Encoder::getVelocity(){
-  int l_distanceChange = getRotation() - m_previousRotation;
-  int l_timeChange = m_timer.getTime() - m_previousTime;
+int RedEncoder::getVelocity(){
+  double l_distanceChange = getRotation() - m_previousRotation;
+  double l_timeChange = (m_timer.getTime() - m_previousTime) / 1000.0;
 
   m_velocity = l_distanceChange / l_timeChange;
 
@@ -32,16 +33,16 @@ int Encoder::getVelocity(){
   return m_velocity;
 }
 
-int Encoder::defineGUI(){
+int RedEncoder::defineGUI(){
   return 0;
 }
 
-int Encoder::changePort(const short p_port){
+int RedEncoder::changePort(const short p_port){
   m_port = p_port;
   return 0;
 }
 
-int Encoder::defineGUI(graphicalInterface& p_gui, std::string p_returnScreen){
+int RedEncoder::defineGUI(graphicalInterface& p_gui, std::string p_returnScreen){
   p_gui.addScreen(m_name);
   p_gui.addLabel(m_name, 200, 10, redText, m_name);
   p_gui.addRectangle(m_name, 0, 0, 480, 40, whiteText);
@@ -49,12 +50,12 @@ int Encoder::defineGUI(graphicalInterface& p_gui, std::string p_returnScreen){
   p_gui.addLabel(m_name, 20, 50, whiteText, "Zero Position: %d", &m_zeroPosition);
   p_gui.addLabel(m_name, 20, 80, whiteText, "Previous Time: %d", &m_previousTime);
   p_gui.addLabel(m_name, 20, 110, whiteText, "Previous Rotation: %d Deg", &m_previousRotation);
-  p_gui.addLabel(m_name, 20, 140, whiteText, "Rotation: %d Deg", (std::function<int()>)std::bind(&Encoder::getRotation, this));
-  p_gui.addLabel(m_name, 20, 170, whiteText, "Velocity: %d", (std::function<int()>)std::bind(&Encoder::getVelocity, this));
+  p_gui.addLabel(m_name, 20, 140, whiteText, "Rotation: %d Deg", (std::function<int()>)std::bind(&RedEncoder::getRotation, this));
+  p_gui.addLabel(m_name, 20, 170, whiteText, "Velocity: %d", (std::function<int()>)std::bind(&RedEncoder::getVelocity, this));
 
   p_gui.addButton(m_name, 0, 160, 200, 150, 20);
   p_gui.addButtonAction(m_name, 0, "Go Back", p_returnScreen);
 
-  p_gui.addRelationship(m_name, (std::function<bool()>)std::bind(&Encoder::isConnected, this), "No_Device", true);
+  p_gui.addRelationship(m_name, (std::function<bool()>)std::bind(&RedEncoder::isConnected, this), "No_Device", false);
   return 0;
 }
