8b1c7b2ecc45a969ac6233e97ee96495fb8b53d8
Corey <ckschanker@gmail.com>
2020-02-06 12:34:38 -0700

    Working on Arm V2

--- src/robot/subsystems/armClassV2.cpp
+++ src/robot/subsystems/armClassV2.cpp
@@ -1,6 +1,13 @@
-// #include "robot/subsystems/armClassV2.hpp"
-// #include "robotClass.hpp"
-//
-// ArmV2::ArmV2(Robot& p_robot) : m_robot(p_robot){
-//
-// }
+#include "robot/subsystems/armClassV2.hpp"
+#include "robotClass.hpp"
+
+ArmV2::ArmV2(Robot& p_robot) : m_robot(p_robot){
+
+}
+
+int ArmV2::task(){
+  switch (m_robot.getRobotMode()) {
+    case /* value */:
+  }
+  return 0;
+}
